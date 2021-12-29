e338bc6588c6aa2525b9aed2d25acb827bdac0c4
olumide <ogundeleolumuyiwaolumide@gmail.com>
2021-12-30 00:52:18 +0100

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,35 @@
+console.log('Client Side Javascript is loading...')
+
+
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#mess-1')
+const messageTwo = document.querySelector('#mess-2')
+
+messageOne.textContent = 'Loading...'
+messageTwo.textContent = ''
+
+weatherForm.addEventListener('submit',(e) => {
+    e.preventDefault()
+    const location = search.value
+    if(!location){
+        console.log('Invalid user input')
+    }else{
+        fetch('http://localhost:3000/weather?address='+location).then((response) => {
+            response.json().then(data => {
+                console.log(data)
+                if(data.error){
+                     messageOne.textContent = data.error
+                }else{
+                    messageOne.textContent = data.forecast
+                    messageTwo.textContent = data.location
+                }
+                
+            })
+        }).catch((error) => {
+            console.log(error)
+        })
+    }
+    
+})
---
+++ src/utils/foreCast.js
@@ -0,0 +1,20 @@
+const request = require('request')
+
+const foreCast = (latitude,longitude,callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=79b17ca5710fa8e83a851fb57a5f1909&query='+latitude+','+longitude+'&units=f'
+
+    request({url : url, json: true},(error,response) => {
+        if(error){
+            callback("unable to connect to weather serrvices ", undefined)
+        }else if(response.body.error){
+            callback('Unable to get search result ',undefined);
+        }else{
+            callback(undefined, 
+                'It is currently '+ response.body.current.temperature+ ' degress out and It feels like ' + response.body.current.feelslike + ' degrees out'
+            )
+        }
+    })
+}
+
+
+module.exports = foreCast
